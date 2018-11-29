143827d13a0e9fb4349510cc8193d268311d9ab2
richard <richard@stp-er-10-129-21-53.wireless.umn.edu>
2018-11-29 12:42:40 -0600

    <adding imports to app.js>

--- views/react-warehouse-app/src/App.js
+++ views/react-warehouse-app/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+import Product from '../components/Product';
+import Row from '../components/Row';
+import Section from '../components/Section';
+import Warehouse from '../components/Warehouse';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <Navbar></Navbar>
+        
+
+      </div>
     );
   }
 }
