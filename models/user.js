1e790d9ae0a4e81150486ff17dbd7c1de048fa67
Parker <theparkinator56@gmail.com>
2018-11-30 13:30:25 -0600

    commiting package.json

--- models/user.js
+++ models/user.js
@@ -1,18 +1,25 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+    mongoose.connect('mongodb://localhost/warehouse-test')
+const passportLocalMongoose = require('passport-local-mongoose');
 
-var Schema = mongoose.Schema
+let db = mongoose.connection;
+let Schema = mongoose.Schema
 
-var UserSchema = new Schema({
+let UserSchema = new Schema({
     Username: {
-        type: Text,
+        type: String,
         required: true
     },
     Password: {
-        type: Text,
+        type: String,
         required: true
     }
 })
+UserSchema.plugin(passportLocalMongoose);
+let User = mongoose.model("User", UserSchema)
 
-var User = mongoose.model("User", UserSchema)
+module.exports.createUser = function(newUser) {
+    newUser.save(); 
+}
 
 module.exports = User
