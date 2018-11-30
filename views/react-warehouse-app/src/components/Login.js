8ff7fe112cdba0d10cf46d77a09140cc13f4d5a9
ehickstein <38333223+ehickstein@users.noreply.github.com>
2018-11-30 10:35:13 -0600

    Added Redux and Front-end Routes

---
+++ views/react-warehouse-app/src/components/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import render from 'react-router-dom';
+
+class Login extends Component {
+
+  
+
+}
+
+export default Login;
---
+++ views/react-warehouse-app/src/components/Row.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Row = () => {
+
+}
+
+
+
+
+export default Row;
