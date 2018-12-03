3d0c0362936fac9f55263ed5cfb2cd33f6b10fff
Parker <theparkinator56@gmail.com>
2018-12-03 13:18:03 -0600

    Made registration with passport

--- models/user.js
+++ models/user.js
@@ -1,25 +1,17 @@
 const mongoose = require("mongoose");
-    mongoose.connect('mongodb://localhost/warehouse-test')
+    mongoose.connect('mongodb://localhost/warehouse-app')
 const passportLocalMongoose = require('passport-local-mongoose');
 
 let db = mongoose.connection;
 let Schema = mongoose.Schema
 
 let UserSchema = new Schema({
-    Username: {
+    username: {
         type: String,
         required: true
     },
-    Password: {
-        type: String,
-        required: true
-    }
 })
 UserSchema.plugin(passportLocalMongoose);
 let User = mongoose.model("User", UserSchema)
-
-module.exports.createUser = function(newUser) {
-    newUser.save(); 
-}
 
 module.exports = User
